e40fc08014209d49b3c08b8e777cdb5306a4829c
CHIH CHIN, TSAI <o660086@tamu.edu>
2020-11-26 21:37:33 -0600

    update

---
+++ client/build/precache-manifest.3d52d692d000ad3adecc2acaa5c664ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa7863d7ff4313c639fee880d7a2476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb621626f27bbdcf8735",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "c197f6111def9dc08c70",
+    "url": "/static/js/2.ad571d88.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.ad571d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb621626f27bbdcf8735",
+    "url": "/static/js/main.dd9dd7e2.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
